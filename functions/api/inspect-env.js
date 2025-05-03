11b3cdcabf35af261ad4e02bc90e95521bb93a48
Richard Moore <dickymoore@gmail.com>
2025-05-03 18:56:01 +0100

    Rebuild with preview env vars

--- functions/api/inspect-env.js
+++ functions/api/inspect-env.js
@@ -3,7 +3,6 @@ export async function GET() {
       JSON.stringify({
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
         GITHUB_ID:        process.env.GITHUB_ID,
-        // don’t echo secrets in cleartext, just show if they exist
         GITHUB_CLIENT_SECRET_exists: !!process.env.GITHUB_CLIENT_SECRET,
         GITHUB_SECRET_exists:        !!process.env.GITHUB_SECRET,
       }, null, 2),
